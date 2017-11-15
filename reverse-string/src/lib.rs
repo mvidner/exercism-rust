8a3a876dd0cfff9617ab8dca12349194e2954a01
Martin Vidner <martin@vidner.net>
2017-11-15 11:06:23 +0100

    reverse-string: solved

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,11 @@
-pub fn reverse(_: &str) -> String {
-    unimplemented!()
+pub fn reverse(s: &str) -> String {
+    let mut result = String::new();
+    result.reserve(s.len());
+
+    let mut cs: Vec<char> = s.chars().collect();
+    cs.reverse();
+    for c in cs {
+        result.push(c);
+    }
+    result
 }
