82adf97604aa97d3bce55be42470565e60675f3c
Martin Vidner <martin@vidner.net>
2017-11-21 10:51:51 +0100

    proverb: solved with double reversing

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,26 @@
 pub fn build_proverb(list: Vec<&str>) -> String {
-    unimplemented!()
+    let mut lines: Vec<String> = vec![];
+    let mut i = list.iter().rev().peekable();
+    loop {
+        match i.next() {
+            Some(a) => {
+                match i.peek() {
+                    Some(b) => { lines.push(want_x_lose_y(b, a)); },
+                    None    => { lines.reverse();
+                                 lines.push(want(a)); },
+                }
+            }
+            None => break,
+        }
+    }
+
+    lines.join("\n")
+}
+
+fn want_x_lose_y(x: &str, y: &str) -> String {
+    format!("For want of a {} the {} was lost.", x, y)
+}
+
+fn want(x: &str) -> String {
+    format!("And all for the want of a {}.", x)
 }
