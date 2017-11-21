c42fc3653ec22d1f74439a7f52b94c0c03781cbd
Martin Vidner <martin@vidner.net>
2017-11-21 20:45:49 +0100

    sum-of-multiples: solved

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,15 @@
+#![feature(iter_arith)]
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!()
+    let mut multiples: HashSet<u32> = HashSet::new();
+
+    for f in factors {
+        let mut i = *f;
+        while i < limit {
+            multiples.insert(i);
+            i += *f;
+        }
+    }
+    multiples.iter().sum()
 }
