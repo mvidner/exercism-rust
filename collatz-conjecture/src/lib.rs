61d644fa30a842928b265e8c5d9ab9f67a156656
Martin Vidner <martin@vidner.net>
2017-11-23 13:03:08 +0100

    collatz-conjecture: solved

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,4 +1,22 @@
 // return Ok(x) where x is the number of steps required to reach 1
 pub fn collatz(n: u64) -> Result<u64, &'static str> {
-    unimplemented!()
+    if n == 0 {
+        return Err("The Collatz formula does not work on 0");
+    }
+
+    let mut count = 0;
+    let mut i = n;
+    while i > 1 {
+        i = step(i);
+        count +=1;
+    }
+    Ok(count)
+}
+
+fn step(n: u64) -> u64 {
+    if n % 2 == 0 {
+        n / 2
+    } else {
+        3 * n + 1
+    }
 }
--- collatz-conjecture/tests/collatz-conjecture.rs
+++ collatz-conjecture/tests/collatz-conjecture.rs
@@ -8,25 +8,21 @@ fn test_1() {
 }
 
 #[test]
-#[ignore]
 fn test_16() {
     assert_eq!(Ok(4), collatz(16));
 }
 
 #[test]
-#[ignore]
 fn test_12() {
     assert_eq!(Ok(9), collatz(12));
 }
 
 #[test]
-#[ignore]
 fn test_1000000() {
     assert_eq!(Ok(152), collatz(1000000));
 }
 
 #[test]
-#[ignore]
 fn test_0() {
     assert!(collatz(0).is_err());
 }
